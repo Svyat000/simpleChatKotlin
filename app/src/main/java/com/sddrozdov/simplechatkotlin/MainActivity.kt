a3cfeebf4636ee789ae4d2d1e4529058c8aa42ea
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-03-09 11:54:40 +0300

    add dependencies, and test message

--- app/src/main/java/com/sddrozdov/simplechatkotlin/MainActivity.kt
+++ app/src/main/java/com/sddrozdov/simplechatkotlin/MainActivity.kt
@@ -1,28 +1,70 @@
 package com.sddrozdov.simplechatkotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.sddrozdov.simplechatkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding ?: throw IllegalStateException("Binding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        WindowInsetUtil.applyWindowInsets(binding.root)
+
 
         val database = Firebase.database
         val myRef = database.getReference("messages")
 
-        myRef.setValue("Test")
+        binding.sendMessage.setOnClickListener {
+            myRef.setValue(binding.editTextText.text.toString())
+        }
+        onChangeListener(myRef)
 
+
+    }
+    private fun onChangeListener(dataBaseRef : DatabaseReference){
+        dataBaseRef.addValueEventListener(
+            object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    binding.apply {
+                        messageView.append("\n")
+                        messageView.append(snapshot.value.toString())
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+        )
+    }
+}
+
+
+object WindowInsetUtil {
+    fun applyWindowInsets(view: View) {
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
